705b19973a0b568ceec60fc5782103ca4f017169
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2025-03-07 08:27:54 +0700

    First commit

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Muhammad Imam";
+
+function getMyIdentity() {
+  let localVariable = 2211104077;
+
+  console.log(`Nama saya ${globalVarible} nim ${localVariable}`);
+}
+
+console.log(globalVarible);
+// console.log(localVariable)
+
+getMyIdentity();
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable.js
@@ -0,0 +1,12 @@
+var name = "Muhammad Imam";
+const nim = 2211104077;
+
+console.log(name);
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim);
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`);
+console.log(`Nama saya adalah ${name} nim saya adalah ${nim}`);
+
+var name = "ayam";
+
+let fullName = "Muhammad Imam";
