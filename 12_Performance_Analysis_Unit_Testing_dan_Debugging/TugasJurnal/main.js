ff1c9fb3d20dd05710c66b059598b985eff6aeeb
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2025-05-16 09:52:59 +0700

    perbaiki logika fungsi CariNilaiPangkat untuk menghindari pengulangan kondisi

--- 12_Performance_Analysis_Unit_Testing_dan_Debugging/TugasJurnal/main.js
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/TugasJurnal/main.js
@@ -1,16 +1,13 @@
 function CariNilaiPangkat(a, b) {
   if (b === 0) return 1;
   if (b < 0) return -1;
+  if (b > 10 || a > 100) return -2;
 
   let hasil = 1;
   for (let i = 0; i < b; i++) {
     hasil *= a;
     if (hasil > Number.MAX_SAFE_INTEGER) return -3;
   }
-
-  if (b > 10 || a > 100) return -2;
-
-  return hasil;
 }
 
 function tampilkanHasil() {
